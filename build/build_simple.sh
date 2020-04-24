f82e2402ecfaacb68ff4c867aa08294412eb99d0
Michael Johns <mjohns@mjohns-macbookpro.roam.corp.google.com>
2020-04-24 17:21:40 -0400

    add build dir

---
+++ build/build_simple.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Building"
+g++ -std=c++17 ../src/*.cc ../src/util/*.cc -I../src -I../src/util -o dactyl
+if [ $? -ne 0 ]; then
+  echo "Failed to build"
+  exit 1
+fi
+echo "Running"
+./dactyl && echo "Done"
